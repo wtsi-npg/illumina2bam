ca6217959b99fa5ea2577eacebcf4c35df463221
gq1 <gq1@sanger.ac.uk>
2011-05-05 10:46:31 +0100

    add license information

--- test/illumina/LaneTest.java
+++ test/illumina/LaneTest.java
@@ -1,7 +1,23 @@
 /*
- * this is the test class for Lane
+ * Copyright (C) 2011 GRL
+ *
+ * This library is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This is the test class for Lane
  *
  */
+
 package illumina;
 
 import net.sf.samtools.SAMReadGroupRecord;
@@ -21,7 +37,7 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author gq1
+ * @author Guoying Qi
  */
 public class LaneTest {
 
