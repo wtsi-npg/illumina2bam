4b71e0b509cc6041150c421aaf858fb4942546c4
gq1 <gq1@sanger.ac.uk>
2011-08-18 14:56:17 +0100

    new class splitting bam by read group

---
+++ src/illumina/SplitBamByReadGroup.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright (C) 2011 GRL
+ *
+ * This library is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * 
+ */
+package illumina;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import net.sf.picard.cmdline.Option;
+import net.sf.picard.cmdline.StandardOptionDefinitions;
+import net.sf.picard.cmdline.Usage;
+import net.sf.picard.io.IoUtil;
+import net.sf.picard.util.Log;
+import net.sf.samtools.SAMFileHeader;
+import net.sf.samtools.SAMFileReader;
+import net.sf.samtools.SAMFileWriter;
+import net.sf.samtools.SAMFileWriterFactory;
+import net.sf.samtools.SAMReadGroupRecord;
+import net.sf.samtools.SAMRecord;
+
+/**
+ *
+ * @author Guoying Qi
+ */
+public class SplitBamByReadGroup extends Illumina2bamCommandLine {
+    
+    private final Log log = Log.getInstance(BamMerger.class);
+    
+    private final String programName = "SplitBamByReadGroup";
+    
+    private final String programDS = "Split a BAM file into multiple BAM files based on ReadGroup. "
+            + "Headers are a copy of the original file, removing @RGs where IDs match with the other ReadGroup IDs";
+   
+    @Usage(programVersion= version)
+    public final String USAGE = this.getStandardUsagePreamble() + this.programDS + ". "; 
+       
+    @Option(shortName= StandardOptionDefinitions.INPUT_SHORT_NAME, doc="The input SAM or BAM file with multiple read groups.")
+    public File INPUT;
+    
+    @Option(shortName= "O", doc="The prefix for output bam/sam file.")
+    public String OUTPUT_PREFIX;
+    
+    @Option(shortName= "TRIM", doc="The common RG name head to trim from the output file name.", optional=true)
+    public String OUTPUT_COMMON_RG_HEAD_TO_TRIM;
+
+    @Override
+    protected int doWork() {
+        
+        log.info("Checking input file");
+        IoUtil.assertFileIsReadable(INPUT);
+        
+
+        log.info("Open input file to split: " + INPUT.getName());
+        final SAMFileReader in  = new SAMFileReader(INPUT);
+        
+        final SAMFileHeader header = in.getFileHeader();
+        final List<SAMReadGroupRecord> readGroupList = header.getReadGroups();
+        
+        if(readGroupList.isEmpty()){
+            log.error("There is no Read Group information in the input file: " + INPUT);
+            return 1;
+        }
+
+        
+        log.info("Open a list of output file based on the read groups in the input header");        
+        HashMap<String, SAMFileWriter> outputFileList = new HashMap<String, SAMFileWriter>();
+    
+        for(SAMReadGroupRecord rg: readGroupList){
+            
+            String id = rg.getId();
+            String idForFileName = id;
+            if(this.OUTPUT_COMMON_RG_HEAD_TO_TRIM != null){
+                if(!idForFileName.startsWith(this.OUTPUT_COMMON_RG_HEAD_TO_TRIM)){
+                   log.error("Read goup ID not starting with the given common prefix");                     
+                }
+                idForFileName = idForFileName.replaceFirst(this.OUTPUT_COMMON_RG_HEAD_TO_TRIM, "");
+            }
+            
+            String filename = this.OUTPUT_PREFIX + idForFileName + ".";
+            if(in.isBinary()){
+                filename += "bam";
+            }else{
+                filename += "sam";
+            }            
+            File outputFile = new File(filename);
+            IoUtil.assertFileIsWritable( outputFile );
+            
+            SAMFileHeader newHeader = header.clone();
+            List<SAMReadGroupRecord> newRGList = new ArrayList();
+            newRGList.add(rg);
+            
+            newHeader.setReadGroups(newRGList);
+
+            this.addProgramRecordToHead(newHeader, this.getThisProgramRecord(programName, programDS));
+            
+            final SAMFileWriter out = new SAMFileWriterFactory().makeSAMOrBAMWriter(newHeader,  true, outputFile);
+            
+            outputFileList.put(id, out);
+        }
+        
+        
+        log.info("Spliting the input file now");
+        for(SAMRecord rec: in){
+            String rgId = (String) rec.getAttribute("RG");
+            outputFileList.get(rgId).addAlignment(rec);
+        }
+        
+        
+        log.info("Close the output files");
+        for(SAMFileWriter out : outputFileList.values()){
+            out.close();
+        }
+        
+        log.info("Splitting finished");
+        
+        return 0;
+    }
+    
+    public static void main(final String[] args) {
+        
+        System.exit(new SplitBamByReadGroup().instanceMain(args));
+    }   
+}
---
+++ test/illumina/SplitBamByReadGroupTest.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package illumina;
+
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.util.TimeZone;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Guoying Qi
+ */
+public class SplitBamByReadGroupTest {
+    
+    SplitBamByReadGroup splitter = new SplitBamByReadGroup();
+    
+    public SplitBamByReadGroupTest() {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
+    }
+
+    /**
+     * Test of instanceMain method and program record.
+     */
+    @Test
+    public void testMain() throws IOException {
+        System.out.println("instanceMain");
+        
+        String[] args = {
+            "I=testdata/decode/6551_8.sam",
+            "O=testdata/6551_8_split",
+            "TRIM=1",
+            "CREATE_MD5_FILE=true",
+            "TMP_DIR=testdata/",
+            "VALIDATION_STRINGENCY=SILENT"
+        };
+
+        splitter.instanceMain(args);
+        
+        assertEquals(splitter.getCommandLine(), "illumina.SplitBamByReadGroup INPUT=testdata/decode/6551_8.sam OUTPUT_PREFIX=testdata/6551_8_split OUTPUT_COMMON_RG_HEAD_TO_TRIM=1 TMP_DIR=testdata VALIDATION_STRINGENCY=SILENT CREATE_MD5_FILE=true    VERBOSITY=INFO QUIET=false COMPRESSION_LEVEL=5 MAX_RECORDS_IN_RAM=500000 CREATE_INDEX=false"); 
+    
+        File splitFile1 = new File("testdata/6551_8_split#1.sam");
+        splitFile1.deleteOnExit();
+        assertTrue(splitFile1.exists());
+
+        File md5File1 = new File("testdata/6551_8_split#1.sam.md5");
+        md5File1.deleteOnExit();
+        BufferedReader md5Stream1 = new BufferedReader(new FileReader(md5File1));
+        String md5 = md5Stream1.readLine();
+        assertEquals(md5, "6918f014a55682f836c77fcf44a3ba75");
+        
+        File splitFile9 = new File("testdata/6551_8_split#9.sam");
+        splitFile9.deleteOnExit();
+        assertTrue(splitFile9.exists());
+
+        File md5File9 = new File("testdata/6551_8_split#9.sam.md5");
+        md5File9.deleteOnExit();
+        BufferedReader md5Stream9 = new BufferedReader(new FileReader(md5File9));
+        md5 = md5Stream9.readLine();
+        assertEquals(md5, "4144376830c02b82b7771381887e58e8");
+    
+    }
+}
