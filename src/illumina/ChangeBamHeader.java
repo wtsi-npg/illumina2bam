794ecd5cd15bd19c6df34a0f88ef566666dca8e6
Guoying Qi <gq1@sanger.ac.uk>
2011-10-10 14:50:47 +0100

    new tool ChangeBamHeader

---
+++ src/illumina/ChangeBamHeader.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (C) 2011 GRL
+ *
+ * This library is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * 
+ */
+package illumina;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import net.sf.picard.cmdline.Option;
+import net.sf.picard.cmdline.StandardOptionDefinitions;
+import net.sf.picard.cmdline.Usage;
+import net.sf.picard.io.IoUtil;
+import net.sf.picard.util.Log;
+import net.sf.samtools.SAMFileHeader;
+import net.sf.samtools.SAMFileReader;
+import net.sf.samtools.SAMFileWriter;
+import net.sf.samtools.SAMFileWriterFactory;
+import net.sf.samtools.SAMProgramRecord;
+import net.sf.samtools.SAMRecord;
+
+/**
+ *
+ * @author Guoying Qi
+ */
+public class ChangeBamHeader extends Illumina2bamCommandLine {
+    
+    private final Log log = Log.getInstance(ChangeBamHeader.class);
+    
+    private final String programName = "ChangeBamHeader";
+    
+    private final String programDS = "Add extra PGs into bam header";
+   
+    @Usage(programVersion= version)
+    public final String USAGE = this.getStandardUsagePreamble() + this.programDS + ". "; 
+ 
+    
+    @Option(shortName=StandardOptionDefinitions.INPUT_SHORT_NAME, doc="The input SAM or BAM file.")
+    public File INPUT;
+    
+    @Option(shortName=StandardOptionDefinitions.OUTPUT_SHORT_NAME, doc="The ouput SAM or BAM file. ")
+    public File OUTPUT;
+    
+    @Option(doc="The extra PG with fields separated by tab for bam header including ID, PN and CL etc, PP id will be reset.")
+    public final List<String> PG = new ArrayList<String>();
+
+    @Override
+    protected int doWork() {
+        
+        this.log.info("Checking input and output file");
+        IoUtil.assertFileIsReadable(INPUT);
+        IoUtil.assertFileIsWritable(OUTPUT);
+        
+        log.info("Open input file: " + INPUT.getName());
+        final SAMFileReader in  = new SAMFileReader(INPUT);
+        
+        final SAMFileHeader header = in.getFileHeader();
+        final SAMFileHeader outputHeader = header.clone();
+
+        for(String pg_fields: this.PG){
+            SAMProgramRecord pg = this.getProgramRecordFromString(pg_fields);
+            this.addProgramRecordToHead(outputHeader, pg);
+        }
+
+        this.addProgramRecordToHead(outputHeader, this.getThisProgramRecord(programName, programDS));
+        
+        log.info("Open output file with header: " + OUTPUT.getName());
+        final SAMFileWriter out = new SAMFileWriterFactory().makeSAMOrBAMWriter(outputHeader,  true, OUTPUT);
+        
+        log.info("Writing out all records");
+        for (SAMRecord record : in) {
+                out.addAlignment(record);
+        }
+        
+        out.close();
+        in.close();
+        log.info("Changing header finished: " + this.OUTPUT);
+        
+        return 0;
+    }
+    
+    /**
+     * 
+     * @param pg_fields
+     * @return 
+     */
+    public SAMProgramRecord getProgramRecordFromString(String pg_fields){
+        
+        String []  fields = pg_fields.split("\t");
+        
+        if(fields.length == 0){
+            throw new RuntimeException("There are no PG fields given: " + pg_fields);
+        }
+        
+        HashMap<String, String> fieldsHash = new HashMap<String, String>();
+        
+        for ( String field : fields ) {
+           String [] keyValue =  field.split(":");
+           if(keyValue.length != 2){
+               throw new RuntimeException("PG field must be given in tag name and value in pair separated by :,  " + field );
+           }
+           
+           fieldsHash.put(keyValue[0], keyValue[1]);
+            
+        }
+        
+        String id = fieldsHash.remove("ID");
+
+        if(id==null){
+            throw new RuntimeException("PG ID must be given: " + pg_fields);
+        }
+        
+        SAMProgramRecord pgRecord = new SAMProgramRecord(id);
+        for (Map.Entry<String, String> field : fieldsHash.entrySet()) {
+            pgRecord.setAttribute (field.getKey(), field.getValue());
+        }
+        
+        return pgRecord;
+    }
+    
+    /**
+     * 
+     * @param argv 
+     */
+    public static void main(final String[] argv) {
+        System.exit(new ChangeBamHeader().instanceMain(argv));
+    }
+}
---
+++ test/illumina/ChangeBamHeaderTest.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2011 GRL
+ *
+ * This library is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+package illumina;
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.File;
+import java.util.TimeZone;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * This is the test class for BamReadTrimmer
+ * 
+ * @author Guoying Qi
+ */
+
+public class ChangeBamHeaderTest {
+    
+    ChangeBamHeader changer = new ChangeBamHeader();
+    
+    public ChangeBamHeaderTest() {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
+    }
+    
+    /**
+     * PG must be given in tag name and value in pair and separated by :
+     */
+    @Test(expected = RuntimeException.class)
+    public void testGetProgramRecordFromString1(){
+        changer.getProgramRecordFromString("ID samtools");
+    }
+    
+    /**
+     * PG must be given with ID tag and value
+     */
+    @Test(expected = RuntimeException.class)
+    public void testGetProgramRecordFromString2(){
+        changer.getProgramRecordFromString("PN:samtools VN0.17");
+    } 
+
+    /**
+     * Test of instanceMain method and program record.
+     */
+    @Test
+    public void testMain() throws FileNotFoundException, IOException {
+        
+        System.out.println("instanceMain");
+        
+        String[] args = {
+            "I=testdata/bam/6210_8.sam",
+            "O=testdata/6210_8_header_changed.bam",
+            "PG=ID:samtools_sorting PN=samtools VN=0.1.12a (r862)   CL=samtools sort",
+            "CREATE_MD5_FILE=true",
+            "TMP_DIR=testdata/",
+            "VALIDATION_STRINGENCY=SILENT"
+        };
+
+        changer.instanceMain(args);
+        
+        System.out.println(changer.getCommandLine());
+        assertEquals(changer.getCommandLine(),
+                "illumina.ChangeBamHeader INPUT=testdata/bam/6210_8.sam OUTPUT=testdata/6210_8_header_changed.bam PG=[ID:samtools_sorting PN=samtools VN=0.1.12a (r862)   CL=samtools sort] TMP_DIR=[testdata] VALIDATION_STRINGENCY=SILENT CREATE_MD5_FILE=true    VERBOSITY=INFO QUIET=false COMPRESSION_LEVEL=5 MAX_RECORDS_IN_RAM=500000 CREATE_INDEX=false"
+                    );
+    }
+    
+    /**
+     * Test output bam MD5
+     */
+    @Test
+    public void testOutputBam() throws FileNotFoundException, IOException {
+        System.out.println("checking bam md5");
+        File newBamFile = new File("testdata/6210_8_header_changed.bam");
+        newBamFile.deleteOnExit();
+
+        File md5File = new File("testdata/6210_8_header_changed.bam.md5");
+        md5File.deleteOnExit();
+        BufferedReader md5Stream = new BufferedReader(new FileReader(md5File));
+        String md5 = md5Stream.readLine();
+
+        assertEquals(md5, "fbffa18b75b6c83b97d49ad958fb9aca");
+    }
+}
