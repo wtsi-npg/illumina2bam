58eba5076b610df269b5bc82a39015b8c50cd3a5
gq1 <gq1@sanger.ac.uk>
2012-05-08 14:29:11 +0100

    version number change from 0.04 to 1.00 and picard updates from 1.67 to 1.68

--- src/uk/ac/sanger/npg/picard/PicardCommandLine.java
+++ src/uk/ac/sanger/npg/picard/PicardCommandLine.java
@@ -32,7 +32,7 @@ import net.sf.samtools.SAMProgramRecord;
  */
 public abstract class PicardCommandLine extends CommandLineProgram {
     
-    public static final String version = "0.04";
+    public static final String version = "1.00";
     
     /**
      * Generate Program Record for this program itself
